b26f98e773f76dced461847d9ca8405a29b8dd21
Madhav Rajan <madh.rajan91@gmail.com>
2020-01-05 23:36:01 -0500

    wrapping things

--- models.py
+++ models.py
@@ -33,16 +33,6 @@ class DBHelper:
     def close():
         db.session.close()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-#def setup_db(app, database_path=database_path):
-    # app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    #db.app = app
-    #db.init_app(app)
-    #db.create_all()
 
 class Base(db.Model):
     __abstract__ = True
